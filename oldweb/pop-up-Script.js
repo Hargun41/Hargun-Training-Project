3585a64168105662ebef73abd8c368c88f320765
Hargun <121561127+Hargun41@users.noreply.github.com>
2025-08-12 22:34:47 +0530

    Delete oldweb directory

--- oldweb/pop-up-Script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector("#close-popup").addEventListener("click", function(){
-    document.querySelector(".pop-up").style.display = "none";
-    document.querySelector(".overlay").style.display = "none";
-});
